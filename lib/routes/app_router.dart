68afb23008a7481e95cbfe969b686ed4519366af
dduranuceva <dduran@uceva.edu.co>
2025-09-30 19:05:34 -0500

    Añadir rutas y vistas para Future e Isolate en la aplicación Flutter

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -3,7 +3,9 @@ import 'package:parqueadero_2025_g2/views/ciclo_vida/ciclo_vida_screen.dart';
 import 'package:parqueadero_2025_g2/views/paso_parametros/detalle_screen.dart';
 import 'package:parqueadero_2025_g2/views/paso_parametros/paso_parametros_screen.dart';
 
+import '../views/future/future_view.dart';
 import '../views/home/home_screen.dart';
+import '../views/isolate/isolate_view.dart';
 
 final GoRouter appRouter = GoRouter(
   routes: [
@@ -35,6 +37,18 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: '/ciclo_vida',
       builder: (context, state) => const CicloVidaScreen(),
+    ),
+    //!Ruta para FUTURE
+    GoRoute(
+      path: '/future',
+      name: 'future',
+      builder: (context, state) => const FutureView(),
+    ),
+    //!Ruta para ISOLATE
+    GoRoute(
+      path: '/isolate',
+      name: 'isolate',
+      builder: (context, state) => const IsolateView(),
     ),
   ],
 );
